e0bcf5f0cf3772f4a62e2652a1588093b8e22e38
Tadashi Saito <tad.a.digger@gmail.com>
2025-03-08 19:03:45 +0900

    WIP

---
+++ src/Schaf-prelude.scm
@@ -0,0 +1,15 @@
+(define import list)
+(define scheme list)
+(define base #f)
+(define write #f)
+(define time #f)
+
+(define (flush-output-port . l)
+  #f)
+
+(define (jiffies-per-second)
+  1)
+
+(define current-second _cputime)
+
+(define current-jiffy _cputime)
